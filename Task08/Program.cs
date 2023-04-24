ed1043ead7754d2145b633e213b4c3f492f5a0dc
Anastasiya Falkovskaya <anastasia.ipm@gmail.com>
2023-04-24 15:38:58 +0300

    Решили задачу 8 из домашнего задания

---
+++ Task08/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до {0}:", N);
+for (int i = 2; i <= N; i += 2) 
+Console.Write("{0} ", i);
