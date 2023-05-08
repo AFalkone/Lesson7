b45152d8a1a531c2513de9fcc8bf52ea38e23a2a
Anastasiya Falkovskaya <anastasia.ipm@gmail.com>
2023-05-08 17:01:04 +0300

    Добавлена задача 20

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -6,28 +6,28 @@
 // A (7,-5); B (1,-1) -> 7,21
 
 
-// Console.WriteLine("Введите координаты точки");
-// Console.Write("X: ");
-// int xCoordinate = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Y: ");
-// int yCoordinate = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки A");
+Console.WriteLine("x = ");
+int xA = Convert.ToInt32(Console.ReadLine());
+Console.Write("y = ");
+int yA = Convert.ToInt32(Console.ReadLine());
 
-// int quarter = Quarter (xCoordinate, yCoordinate);
-// string result = quarter > 0 
-//                 ? $"Указанные координаты соответствуют четверти -> {quarter}"
-//                 : "Введены некорректные корректные координаты";
-// Console.WriteLine(result);  
+Console.WriteLine("Введите координаты точки A");
+Console.WriteLine("x = ");
+int xB = Convert.ToInt32(Console.ReadLine());
+Console.Write("y = ");
+int yB = Convert.ToInt32(Console.ReadLine());
 
-int Quarter(int x, int y)
+double distance = Distance (xA, yA, xB, yB);
+
+Console.WriteLine(Math.Round(distance, 2 , MidpointRounding.ToZero));
+
+double Distance(int x1, int y1, int x2, int y2 )
 {
-    if(x > 0 && y > 0) return 1;
-    if(x < 0 && y > 0) return 2;
-    if(x < 0 && y < 0) return 3;
-    if(x > 0 && y < 0) return 4;
-    return 0;
+    int katet1 = x1 - x2;
+    int katet2 = y2 - y1;
+    double hypo = Math.Sqrt(katet1 * katet1 + katet2 * katet2);
+    return hypo;
 }
 
-double d = Math.Sqrt(5);
-double num = 5.099786232;
-double numRound = Math.Round(num, 2);
-Console.WriteLine(numRound);
+
